20a5e1ba3e8e1eb95d7455d235869e47765d3f86
Andrii <tap.press.swap@gmail.com>
2025-04-19 23:03:26 +0300

    finish lab6

--- src/main/java/vasin/security25/config/SecurityConfig.java
+++ src/main/java/vasin/security25/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package vasin.security25.config;
 
+import org.springframework.aop.Advisor;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInterceptor;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +22,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public static Advisor preAuthorizeMethodInterceptor() {
+        return AuthorizationManagerBeforeMethodInterceptor.preAuthorize();
+    }
+
+    @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
@@ -28,8 +38,8 @@ public class SecurityConfig {
         http.csrf(csrf ->csrf.disable())
                 .authorizeHttpRequests( req ->
                         req.requestMatchers("/index.html").permitAll()
-                                .requestMatchers("/api/v1/rooms/hello/admin").hasRole("ADMIN")
-                                .requestMatchers("/api/v1/rooms/hello/superadmin").hasRole("SUPERADMIN")
+//                                .requestMatchers("/api/v1/rooms/hello/admin").hasRole("ADMIN")
+//                                .requestMatchers("/api/v1/rooms/hello/superadmin").hasRole("SUPERADMIN")
                                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
